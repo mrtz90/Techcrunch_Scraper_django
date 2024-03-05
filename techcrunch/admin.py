4009cd150f4bd5321e67f343cfd8858709c3c3a4
mrtz90 <morteza48.ahmadi@gmail.com>
2024-03-06 00:56:22 +0330

    add code for admin panel to project

--- techcrunch/admin.py
+++ techcrunch/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
 
-# Register your models here.
+from import_export.resources import ModelResource
+from import_export.admin import ExportMixin
+
+from .models import Author, Article, Category, DailySearchResult, UserSearchResult, KeyWordSearched
+
+
+@admin.register(Article)
+class ArticleAdmin(ExportMixin, admin.ModelAdmin):
+    list_display = [
+        'id', 'title', 'display_authors', 'summary', 'category',
+        'content', 'article_created_at', 'image_path'
+    ]
+    list_display_links = ['id', 'title']
+    list_filter = ['title', 'author', 'category']
+
+    @staticmethod
+    def display_authors(obj):
+        return ", ".join([author.name for author in obj.author.all()])
+
+
+@admin.register(Author)
+class AuthorAdmin(ExportMixin, admin.ModelAdmin):
+    list_display = [
+        'id', 'name'
+    ]
+
+
+@admin.register(Category)
+class CategoryAdmin(ExportMixin, admin.ModelAdmin):
+    list_display = [
+        'id', 'title'
+    ]
+
+
+@admin.register(KeyWordSearched)
+class KeywordSearchedAdmin(ExportMixin, admin.ModelAdmin):
+    list_display = [
+        'id', 'keyword',
+    ]
+
+
+@admin.register(DailySearchResult)
+class DailySearchResultAdmin(ExportMixin, admin.ModelAdmin):
+    list_display = [
+        'id', 'category', 'created_at'
+    ]
+
+
+@admin.register(UserSearchResult)
+class UserSearchedAdmin(ExportMixin, admin.ModelAdmin):
+    list_display = [
+        'id', 'keyword',
+    ]
